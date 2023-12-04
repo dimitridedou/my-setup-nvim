15c2e175d9da294e2e74da7aa69f9b4c109cf9b9
Dimitris-Dedousis <72694765+Dimitris-Dedousis@users.noreply.github.com>
2023-12-04 18:54:53 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,2 +1,3 @@
 require("import.lazy")
-vim.opt.relativenumber = true -- Εμφανίζει την αρίθμηση γραμμών 
+vim.opt.number = true -- Εμφανίζει την αρίθμηση γραμμών 
+vim.opt.clipboard:append("unnamedplus") -- Ενεργοποίηση αντιγραφής στο clipboard
